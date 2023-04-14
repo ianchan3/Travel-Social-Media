7bab109fc04ccaadba1e4ec4eb7b126541df07e7
ianchan3 <ianchan3138@gmail.com>
2023-04-14 14:44:27 -0700

    Tested Use Selector to grab Posts from PostReducer

--- client/src/components/Posts/Posts.jsx
+++ client/src/components/Posts/Posts.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Post from './Post/Post';
 import useStyles from './styles';
 
 export default function Posts() {
+  const posts = useSelector((state) => state.postsReducer);
   const classes = useStyles();
+  console.log(posts)
   return (
     <>
       <h1>Posts</h1>
