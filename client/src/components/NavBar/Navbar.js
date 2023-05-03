2d659cde7ff4d08aed30c091aaa3c65255768aad
ianchan3 <ianchan3138@gmail.com>
2023-05-02 19:54:38 -0700

    Google Logout Functionality Implemented

--- client/src/components/NavBar/Navbar.js
+++ client/src/components/NavBar/Navbar.js
@@ -1,13 +1,29 @@
 import React, { useState, useEffect } from "react";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { useDispatch } from "react-redux";
 import { AppBar, Typography, Toolbar, Button, Avatar } from '@material-ui/core';
 import memories from '../../images/memories.png';
 import useStyles from './styles';
 
 export default function Navbar() {
   const classes = useStyles();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const location = useLocation();
   const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+
+  const logout = () => {
+    dispatch({ type: 'LOGOUT'});
+    navigate("/auth");
+    setUser(null);
+  }
   console.log(user)
+
+  useEffect(() => {
+    const token = user?.token;
+
+    setUser(JSON.parse(localStorage.getItem('profile')));
+  }, [location]);
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
       <div className={classes.brandContainer}>
@@ -19,7 +35,7 @@ export default function Navbar() {
           <div className={classes.profile}>
             <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
             <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
-            <Button variant="contained" className={classes.logout} color="secondary">Logout</Button>
+            <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
           </div>
         ) : (
           <Button component={Link} to = "/auth" variant="contained" color="primary">Sign In</Button>
