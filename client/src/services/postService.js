100bc78f440f61ca36ba800a9bd3e9d6b3565980
ianchan3 <ianchan3138@gmail.com>
2023-05-12 15:16:18 -0700

    Deploy to Heroku

--- client/src/services/postService.js
+++ client/src/services/postService.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API = axios.create({ baseURL: 'http://localhost:4000' })
+const API = axios.create({ baseURL: 'https://ianchantravelwebsite.herokuapp.com/' })
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
