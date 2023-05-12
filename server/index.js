e3a81e51225952ec58e4cafed89b26e80c00e131
ianchan3 <ianchan3138@gmail.com>
2023-05-12 15:08:25 -0700

    Deploy To Heroku

--- server/index.js
+++ server/index.js
@@ -16,9 +16,6 @@ app.use(cors());
 app.use('/posts', postRoutes)
 app.use('/users', userRoutes);
 
-app.get('/', (req, res) => {
-  res.send('APP IS RUNNING')
-});
 
 mongoose.connect(process.env.DATABASE_URL)
 .then(() => {
