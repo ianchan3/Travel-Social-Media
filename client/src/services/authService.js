ca20876e1268ad84e9b61c5b04ec81ee22e69e25
ianchan3 <ianchan3138@gmail.com>
2026-01-06 19:57:16 -0800

    Revert "change axios"
    This reverts commit 7e5c4034cc653ffd5b8461feb99dfd5789483653.

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
-const API = axios.create({
-  baseURL: "/.netlify/functions",
-});
+const API = axios.create({ baseURL: process.env.REACT_APP_API_URL });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
