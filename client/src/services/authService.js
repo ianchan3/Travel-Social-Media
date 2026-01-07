071e7992df6c89196df73a8ead399e8c3e17ce1a
ianchan3 <ianchan3138@gmail.com>
2026-01-06 18:34:45 -0800

    Add ./netlify/functions

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API = axios.create({ baseURL: 'http://localhost:4000' })
+const API = axios.create({ baseURL: process.env.REACT_APP_API_URL });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
--- client/src/services/postService.js
+++ client/src/services/postService.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const baseURL = process.env.REACT_APP_API_URL || "/";
-
-export const API = axios.create({ baseURL });
+const API = axios.create({ baseURL: process.env.REACT_APP_API_URL || "/.netlify/functions" });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
