224ebdcfcbadd12752ede5f3ef5c56f9f0e796b6
ianchan3 <ianchan3138@gmail.com>
2026-01-06 19:58:04 -0800

    Revert "Fix"
    This reverts commit afa8917b34ba181bf0e2fe2f19f173dadae94c64.

--- client/src/components/Posts/Posts.jsx
+++ client/src/components/Posts/Posts.jsx
@@ -5,7 +5,7 @@ import Post from './Post/Post';
 import useStyles from './styles';
 
 export default function Posts({ setCurrentId }) {
-  const { posts, isLoading = false } = useSelector(
+  const { posts = [], isLoading = false } = useSelector(
     (state) => state.postsReducer || {}
   );
 
